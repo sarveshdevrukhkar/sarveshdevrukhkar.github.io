b2f5097c115402c471d429fddf27919555c668db
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-05-01 21:24:27 +0530

    Added base version of new portfolio.

---
+++ script/index.js
@@ -0,0 +1,53 @@
+const dynamicText = document.querySelector("#about-tagline");
+const words = [
+  "I am a MERN Stack Developer.",
+  "Passionate Programmer.",
+  "I like to contribute into Open Source.",
+];
+
+// Variables to track the position and deletion status of the word
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typeEffect = () => {
+  const currentWord = words[wordIndex];
+  const currentChar = currentWord.substring(0, charIndex);
+  dynamicText.textContent = currentChar;
+  dynamicText.classList.add("stop-blinking");
+
+  if (!isDeleting && charIndex < currentWord.length) {
+    // If condition is true, type the next character
+    charIndex++;
+    setTimeout(typeEffect, 100);
+  } else if (isDeleting && charIndex > 0) {
+    // If condition is true, remove the previous character
+    charIndex--;
+    setTimeout(typeEffect, 50);
+  } else {
+    // If word is deleted then switch to the next word
+    isDeleting = !isDeleting;
+    dynamicText.classList.remove("stop-blinking");
+    wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+    setTimeout(typeEffect, 1200);
+  }
+};
+
+typeEffect();
+
+/* Show Go To Top Button */
+const goToTopButton = document.querySelector(".go-to-top");
+
+window.addEventListener("scroll", () => {
+  const scrolled = window.scrollY;
+  if (scrolled < 600) {
+    goToTopButton.style.display = "none";
+  }
+  if (scrolled >= 600) {
+    goToTopButton.style.display = "flex";
+  }
+});
+
+goToTopButton.addEventListener("click", (event) => {
+  window.scroll({ top: 0, left: 0, behavior: "smooth" });
+});
