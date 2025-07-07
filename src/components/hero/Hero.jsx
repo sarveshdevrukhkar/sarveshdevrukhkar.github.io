0c8e519450feaa711c9f44c4244d0bfad87848db
sarveshdevrukhkar <sarveshdevrukhkar@outlook.com>
2025-07-07 22:03:38 +0530

    Done with Navbar and Hero section.

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,17 +4,19 @@ import heroImg from "../../assets/logo.png";
 export default function Hero() {
 	return (
 		<div id="hero">
-			<div className="hero-img">
-				<img src={heroImg} alt="Hero Image" id="hero" />
+			{/* Hero Image */}
+			<div className="hero-img-box">
+				<img src={heroImg} alt="Hero Image" id="hero-img" />
 			</div>
+			
+			{/* Hero Name */}
 			<div className="hero-name">
 				<h1>SARVESH DEVRUKHKAR</h1>
 			</div>
-			<div className="hero-headline">
-				<h2>MERN Stack Web Developer</h2>
-			</div>
+
+			{/* Hero Tagline */}
 			<div className="hero-tagline">
-				<h3>Ex. Web Developer Intern @CWD Limited | MERN Stack Web Developer | React.js | Node.js | Express.js | MongoDB | JavaScript | TypeScript | jQuery | Java (DSA) | Git</h3>
+				<h3>Ex. Web Developer Intern @CWD Limited | MERN Stack Web Developer</h3>
 			</div>
 		</div>
 	);
